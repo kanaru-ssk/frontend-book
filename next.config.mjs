6f5303d765f315ab697345e6e119efbba3c77d6c
kanaru-ssk <2019january.0131@gmail.com>
2024-01-26 00:39:30 +0900

    add syntax highlight

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import createMDX from "@next/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  reactStrictMode: true,
+};
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: "dark-plus",
+        },
+      ],
+    ],
+  },
+});
+
+export default withMDX(nextConfig);
