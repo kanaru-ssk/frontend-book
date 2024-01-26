0fca3b5c86c7dbe9c9038676a70b710f82cf1a32
kanaru-ssk <2019january.0131@gmail.com>
2024-01-27 02:15:42 +0900

    Tabにmdxを渡せるよう修正

--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -1,36 +1,46 @@
 "use client";
 
-import { type ReactNode, useState } from "react";
+import { Children, isValidElement, useState, type ReactNode } from "react";
 
-type Props = {
-  tabs: {
-    label: string;
-    content: ReactNode;
-  }[];
+type TabsProps = {
+  children: ReactNode;
 };
+export function Tabs({ children }: TabsProps) {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-export function Tab({ tabs }: Props) {
-  const [activeTab, setActiveTab] = useState(0);
+  const labels = Children.map(children, (element) => {
+    if (isValidElement(element)) {
+      return element.props.label;
+    }
+  }) as string[];
 
   return (
     <div className="shadow">
       <div className="border-b-2 border-neutral-300">
-        {tabs.map((tab, index) => (
+        {labels.map((label, index) => (
           <button
             key={index}
             type="button"
             className={`${
-              index === activeTab
+              index === activeIndex
                 ? "border-black text-black"
                 : "border-neutral-300 text-neutral-500 hover:border-neutral-400 hover:text-neutral-600"
             } m-2 -mb-0.5 w-36 border-b-2 px-9 py-3 text-lg`}
-            onClick={() => setActiveTab(index)}
+            onClick={() => setActiveIndex(index)}
           >
-            {tab.label}
+            {label}
           </button>
         ))}
       </div>
-      <div className="p-4">{tabs[activeTab].content}</div>
+      {Children.toArray(children)[activeIndex]}
     </div>
   );
+}
+
+type TabProps = {
+  label: string;
+  children: ReactNode;
+};
+export function Tab({ children }: TabProps) {
+  return <div className="p-4">{children}</div>;
 }
