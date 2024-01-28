51af3f925339c331a232729d03fbe08f022380fc
yui-szk <yuisuzuki.017@gmail.com>
2024-01-28 23:50:14 +0900

    tabのwidth変更

--- src/components/sync-tab.tsx
+++ src/components/sync-tab.tsx
@@ -53,7 +53,7 @@ function Label({ os }: LabelProps) {
         target === os
           ? "border-black text-black"
           : "border-neutral-300 text-neutral-500 hover:border-neutral-400 hover:text-neutral-600"
-      } m-2 -mb-0.5 w-36 border-b-2 px-8 py-3 text-lg`}
+      } m-2 -mb-0.5 min-w-36 border-b-2 px-8 py-3 text-lg`}
       onClick={() => setTarget(os)}
     >
       {os}
