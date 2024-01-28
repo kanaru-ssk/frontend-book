9610e1a3afa3422e7b3727cee963e05049240c8c
kanaru-ssk <2019january.0131@gmail.com>
2024-01-29 02:39:10 +0900

    リンクテキストが改行されるように修正

--- src/components/footer-link.tsx
+++ src/components/footer-link.tsx
@@ -16,7 +16,7 @@ export function FooterLink({ type, href, title }: Props) {
         type === "prev"
           ? "before:left-4 before:rotate-45 before:border-l-2"
           : "text-right before:right-4 before:-rotate-45 before:border-r-2"
-      } relative rounded px-8 py-5 before:absolute before:top-1/2 before:h-2 before:w-2  before:border-b-2 before:border-neutral-500 hover:bg-neutral-100`}
+      } relative break-all rounded px-8 py-5 before:absolute before:top-1/2 before:h-2 before:w-2  before:border-b-2 before:border-neutral-500 hover:bg-neutral-100`}
     >
       <p className="mb-1 text-sm text-neutral-500">{text[type]}</p>
       <p>{title}</p>
