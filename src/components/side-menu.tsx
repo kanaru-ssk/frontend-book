7a011ff5fc40cbfb58f1180849d13471c0806b5a
yui-szk <yuisuzuki.017@gmail.com>
2024-02-04 17:26:31 +0900

    linkをクリックしたときにサイドメニューを閉じる処理を追加

--- src/components/side-menu.tsx
+++ src/components/side-menu.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import type { ArticleChapter } from "@/libs/load-article-chapters";
+import { useState } from "react";
 
 type Props = {
   articleChapters: ArticleChapter[];
@@ -11,11 +12,14 @@ type Props = {
 export function SideMenu({ articleChapters }: Props) {
   const pathname = usePathname();
   const slug = pathname.split("/").pop();
+  const [open, setOpen] = useState(false);
 
   return (
     <aside>
       <input
         id="menu-button"
+        checked={open}
+        onChange={() => setOpen(!open)}
         type="checkbox"
         className="peer fixed left-1 top-0 z-10 h-16 w-16 cursor-pointer appearance-none before:absolute before:right-5 before:top-8 before:block before:h-0.5 before:w-6 before:-translate-y-1 before:bg-black before:duration-300 after:absolute after:right-5 after:top-8 after:block after:h-0.5 after:w-6 after:translate-y-1 after:bg-black after:duration-300 checked:before:translate-y-0 checked:before:rotate-45 checked:after:translate-y-0 checked:after:-rotate-45 md:left-6 md:hidden"
       />
@@ -30,6 +34,7 @@ export function SideMenu({ articleChapters }: Props) {
               <Link
                 href={chapter.href}
                 data-active={chapter.slug === slug}
+                onClick={() => setOpen(false)}
                 className="my-1 inline-block font-semibold text-neutral-500 hover:text-neutral-600 data-[active=true]:text-blue-500"
               >
                 {chapter.title}
@@ -40,6 +45,7 @@ export function SideMenu({ articleChapters }: Props) {
                     <Link
                       href={section.href}
                       data-active={section.slug === slug}
+                      onClick={() => setOpen(false)}
                       className="ml-1 inline-block border-l border-l-neutral-300 py-1 pl-4 text-neutral-500 hover:border-l-neutral-400 hover:text-neutral-600 data-[active=true]:border-l-blue-500 data-[active=true]:text-blue-500"
                     >
                       {section.title}
