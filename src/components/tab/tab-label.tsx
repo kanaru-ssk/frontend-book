2a2eec3c490e2b4239700f9bef53687153662816
kanaru-ssk <2019january.0131@gmail.com>
2024-02-04 19:05:45 +0900

    TabLabelのフォントサイズ修正

--- src/components/tab/tab-label.tsx
+++ src/components/tab/tab-label.tsx
@@ -12,7 +12,7 @@ export function TabLabel({ label, isActive, onClick }: Props) {
         isActive
           ? "border-blue-600 text-blue-600"
           : "border-neutral-300 text-neutral-500 hover:border-neutral-400 hover:text-neutral-600"
-      } -mb-0.5 border-b-2 p-2 text-lg font-bold`}
+      } -mb-0.5 border-b-2 p-2 font-bold`}
       onClick={onClick}
     >
       {label}
