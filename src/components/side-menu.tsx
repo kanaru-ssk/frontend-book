bae089d7f08771222d427573b2cec9a225752bcb
yui-szk <yuisuzuki.017@gmail.com>
2024-02-04 17:16:48 +0900

    durationを500から300に変更

--- src/components/side-menu.tsx
+++ src/components/side-menu.tsx
@@ -17,13 +17,13 @@ export function SideMenu({ articleChapters }: Props) {
       <input
         id="menu-button"
         type="checkbox"
-        className="peer fixed left-1 top-0 z-10 h-16 w-16 cursor-pointer appearance-none before:absolute before:right-5 before:top-8 before:block before:h-0.5 before:w-6 before:-translate-y-1 before:bg-black before:duration-500 after:absolute after:right-5 after:top-8 after:block after:h-0.5 after:w-6 after:translate-y-1 after:bg-black after:duration-500 checked:before:translate-y-0 checked:before:rotate-45 checked:after:translate-y-0 checked:after:-rotate-45 md:left-6 md:hidden"
+        className="peer fixed left-1 top-0 z-10 h-16 w-16 cursor-pointer appearance-none before:absolute before:right-5 before:top-8 before:block before:h-0.5 before:w-6 before:-translate-y-1 before:bg-black before:duration-300 after:absolute after:right-5 after:top-8 after:block after:h-0.5 after:w-6 after:translate-y-1 after:bg-black after:duration-300 checked:before:translate-y-0 checked:before:rotate-45 checked:after:translate-y-0 checked:after:-rotate-45 md:left-6 md:hidden"
       />
       <label
         htmlFor="menu-button"
         className="fixed top-16 hidden h-full w-full peer-checked:block"
       />
-      <nav className="fixed top-16 z-10 h-[calc(100%-4rem)] w-64 -translate-x-64 overflow-y-auto bg-white px-10 py-10 shadow-2xl duration-500 peer-checked:translate-x-0 md:translate-x-0 md:shadow-none">
+      <nav className="fixed top-16 z-10 h-[calc(100%-4rem)] w-64 -translate-x-64 overflow-y-auto bg-white px-10 py-10 shadow-2xl duration-300 peer-checked:translate-x-0 md:translate-x-0 md:shadow-none">
         <ul>
           {articleChapters.map((chapter) => (
             <li key={chapter.chapterId}>
